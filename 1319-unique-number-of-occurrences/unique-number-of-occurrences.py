69f43c60e9d1a08be9e0e46f85a4263728d199d0
Abhishek Yadav <112726043+Xbectordash@users.noreply.github.com>
2024-03-07 16:35:03 +0530

    Time: 47 ms (18.92%) | Memory: 16.7 MB (94.87%) - LeetSync

--- 1319-unique-number-of-occurrences/unique-number-of-occurrences.py
+++ 1319-unique-number-of-occurrences/unique-number-of-occurrences.py
@@ -3,8 +3,7 @@ class Solution:
         dic = {}
         for i in arr:
            dic[i] = dic.get(i,0)+1
-        if len(dic.values()) == len(set(dic.values())):
-            return True
-        else: return False    
+        return len(dic.values()) == len(set(dic.values()))
+   
 
          
