70520daab033cead2a813bcc8f71e90703896828
Abhishek Yadav <112726043+Xbectordash@users.noreply.github.com>
2024-07-11 16:52:15 +0530

    Time: 38 ms (43.44%) | Memory: 16.7 MB (22.02%) - LeetSync

--- 392-is-subsequence/is-subsequence.py
+++ 392-is-subsequence/is-subsequence.py
@@ -3,7 +3,9 @@ class Solution:
         if s == "" and not t == "":
             return True  
         if s == "" and t == "":
-            return True     
+            return True 
+        if not s == "" and t == "":
+            return False        
 
         l = 0
         r = len(s)-1
