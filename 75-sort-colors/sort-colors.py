6bb499368493ff672146540554f763d401fb8075
Abhishek Yadav <112726043+Xbectordash@users.noreply.github.com>
2024-10-26 12:03:57 +0530

    Time: 3 ms (90.28%) | Memory: 16.7 MB (11.72%) - LeetSync

--- 75-sort-colors/sort-colors.py
+++ 75-sort-colors/sort-colors.py
@@ -1,7 +1,31 @@
 class Solution:
+    def merge_sort(self,arr):
+        if len(arr) <= 1:
+            return arr
+        mid = len(arr)//2    
+        left = self.merge_sort(arr[:mid])
+        right= self.merge_sort(arr[mid:])
+        return self.merge(left,right)
+    def merge(self,left, right):
+        result = []
+        i,j = 0,0
+        while i < len(left) and j < len(right):
+            if left[i] <= right[j]:
+                result.append(left[i])
+                i += 1
+            else:
+                result.append(right[j])
+                j += 1
+        result.extend(left[i:])
+        result.extend(right[j:])    
+        return result        
+
+
+
     def sortColors(self, nums: List[int]) -> None:
         """
         Do not return anything, modify nums in-place instead.
         """
-        return nums.sort()
+        sorted_nums = self.merge_sort(nums)
+        nums[:] = sorted_nums 
         
